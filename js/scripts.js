9b9f441a9b3166e9ac135c6e2d8f6d7e0781b846
Eleanor <emeriwet@uci.edu>
2017-09-27 11:50:11 -0700

    trying highlight current topnav

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@
 }
 */
 
-function() {
-    ('a').each(function()) {
-        if (this.prop('href') == window.location.href) {
-        this.addClass('current');
-        }           
-    }
-}
+$(document).ready(function() { 
+    var str=location.href.toLowerCase(); 
+    $("#topNav li a").each(function() { 
+        if (str.indexOf(this.href.toLowerCase()) > -1) {
+            $("li.current").removeClass("current"); 
+            $(this).parent().addClass("current"); 
+        }
+    });
+})
