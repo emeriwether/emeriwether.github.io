99ebe7e877bfc734db203edf894a08c954aee1da
Eleanor <eleanor@delux.com>
2017-11-10 22:30:11 -0800

    fix javascript and change nav link color for experiment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
     var str=location.href.toLowerCase(); 
-    $("#topNav li a").each(function() { 
+    $("nav ul li a").each(function() { 
         if (str.indexOf(this.href.toLowerCase()) > -1) {
             $("li.current").removeClass("current"); 
             $(this).parent().addClass("current"); 
