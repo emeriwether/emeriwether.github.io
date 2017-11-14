4759875104a9f1e78568e016067a3ea58793115c
Eleanor <eleanor@delux.com>
2017-11-13 20:13:44 -0800

    try to get js to work to highlight active link

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+/*$(document).ready(function() { 
     var str=location.href.toLowerCase(); 
     $("nav ul li a").each(function() { 
         if (str.indexOf(this.href.toLowerCase()) > -1) {
@@ -6,4 +6,11 @@ $(document).ready(function() {
             $(this).parent().addClass("current"); 
         }
     });
-})
+})*/
+
+jQuery(document).ready(function($){
+    var path = window.location.pathname.split("/").pop();
+    if (path == '' ) { path = 'index.html'; }
+    var target = $('nav a[href="'+path+'"]');
+    target.addClass('active');  
+});
