50e3e30b9709f2e9549d6bc429d0b209c044f52a
Eleanor <eleanor@delux.com>
2017-11-13 20:33:06 -0800

    try to get toTop button to show on 490 media query

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-/*$(document).ready(function() { 
-    var str=location.href.toLowerCase(); 
-    $("nav ul li a").each(function() { 
-        if (str.indexOf(this.href.toLowerCase()) > -1) {
-            $("li.current").removeClass("current"); 
-            $(this).parent().addClass("current"); 
-        }
-    });
-})*/
-
-jQuery(document).ready(function($){
-    var path = window.location.pathname.split("/").pop();
-    if (path == '' ) { path = 'index.html'; }
-    var target = $('nav a[href="'+path+'"]');
-    target.addClass('active');  
-});
+function scrollWin() {
+    window.scrollTo(0, 0);
+}
