fd92cbe4a1ddff5030858ff11a6597ac14088eb0
Eleanor <eleanor@delux.com>
2018-03-03 20:43:27 -0800

    new attempt to get active page to be highlighted in nav

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,4 @@ function scrollWin() {
     window.scrollTo(0, 0);
 }
 
-var url = "http://emeriwether.com/index.html".split("/"); //replace string with location.href
-var navLinks = document.getElementsByTagName("li")[0].getElementsByTagName("a");
-//naturally you could use something other than the <nav> element
-var i=0;
-var currentPage = url[url.length - 1];
-for(i;i<navLinks.length;i++) {
-    var lb = navLinks[i].href.split("/");
-    if(lb[lb.length-1] == currentPage) {
-        navLinks[i].className = "current";
-    }
-} 
 
